6d0d81d246426ebb924ce223eae51572c6ec4644
Kriss Andrew <andkri04@csgicorp.com>
2025-01-22 09:52:45 +1000

    Moved test data closer to cases, added in max profit tests

--- Python_NeetCode_150/Easy/Problem_1_Balanced_Binary_Tree/test_solution.py
+++ Python_NeetCode_150/Easy/Problem_1_Balanced_Binary_Tree/test_solution.py
@@ -4,35 +4,30 @@ from Python_NeetCode_150.Easy.Problem_1_Balanced_Binary_Tree.solutions import *
 
 class TestTreeBalance(unittest.TestCase):
         
-        def setUp(self):
-            # Prepare some sample trees for testing
-            self.tree_data_1 = [1, 2, 3, None, None, 4, 5]
-            self.tree_data_2 = [1, 2, 3, None, None, 4, None, 5]
-            self.tree_data_3 = [1, 2, None]
-            self.tree_data_4 = []
-
-            # Creating tree nodes from the sample data
-            self.root_1 = create_tree_from_list(self.tree_data_1)
-            self.root_2 = create_tree_from_list(self.tree_data_2)
-            self.root_3 = create_tree_from_list(self.tree_data_3)
-            self.root_4 = create_tree_from_list(self.tree_data_4)
-
         def test_tree_is_balanced(self):
-            print("Running tests for tree balance")  # Debugging to check if the test function is running
+            print("\nRunning tests for 'tree is balanced'")
 
             # Test case 1 - Balanced Tree
+            self.tree_data_1 = [1, 2, 3, None, None, 4, 5]
+            self.root_1 = create_tree_from_list(self.tree_data_1)
             self.assertTrue(tree_is_balanced_recursive(self.root_1))
             self.assertTrue(tree_is_balanced_iterative(self.root_1))
 
             # Test case 2 - Unbalanced Tree
+            self.tree_data_2 = [1, 2, 3, None, None, 4, None, 5]
+            self.root_2 = create_tree_from_list(self.tree_data_2)
             self.assertFalse(tree_is_balanced_recursive(self.root_2))
             self.assertFalse(tree_is_balanced_iterative(self.root_2))
             
-            # Test case 3
+            # Test case 3 - Balanced Tree
+            self.tree_data_3 = [1, 2, None]
+            self.root_3 = create_tree_from_list(self.tree_data_3)
             self.assertTrue(tree_is_balanced_recursive(self.root_3))
             self.assertTrue(tree_is_balanced_iterative(self.root_3))
 
-            # Test case 3
+            # Test case 4 - Balanced Tree
+            self.tree_data_4 = []
+            self.root_4 = create_tree_from_list(self.tree_data_4)
             self.assertTrue(tree_is_balanced_recursive(self.root_4))
             self.assertTrue(tree_is_balanced_iterative(self.root_4))
 
--- Python_NeetCode_150/Easy/Problem_2_Best_Time_to_Buy_and_Sell_Stock/Solution.py
+++ Python_NeetCode_150/Easy/Problem_2_Best_Time_to_Buy_and_Sell_Stock/Solution.py
@@ -1,20 +1,13 @@
 from typing import List
 
 def maxProfit(prices: List[int]) -> int:
-    
+    if not prices : return 0 # Empty list
+
     lowest = prices[0]
-    daily_profit = 0
     max_profit = 0
 
     for price in prices[1:]:
-        daily_profit = price - lowest
-        if daily_profit > max_profit: max_profit = daily_profit
+        max_profit = max(max_profit, price - lowest)
         if price < lowest:
             lowest = price
-
     return max_profit
-
-if __name__ == '__main__':
-    test = [10, 3, 7, 4, 1] # Expected out come is 4
-    print(maxProfit(test))
-
