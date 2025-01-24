0c03f6c59234179f2ec53bafd6d9d93fd18e8e3b
Kriss Andrew <Kriss1Andrew@gmail.com>
2025-01-24 13:12:33 +1000

    removed common

--- Python_NeetCode_150/Easy/Problem_1_Balanced_Binary_Tree/solutions.py
+++ Python_NeetCode_150/Easy/Problem_1_Balanced_Binary_Tree/solutions.py
@@ -1,20 +1,5 @@
-import sys
-import os
 from typing import Optional
-
-# Print for debugging
-import sys
-import os
-print("solution.py")
-print("Absolute path of test file:", os.path.abspath(__file__))
-print("Directory of test file:", os.path.dirname(os.path.abspath(__file__)))
-print("Parent directory:", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print("Sys path:", sys.path)
-
-# Add the project root directory (Python_NeetCode_150) to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Python_NeetCode_150')))
-
-from Python_NeetCode_150.Common.binary_tree import *
+from .binary_tree import *
 
 def tree_is_balanced_recursive(root: Optional[TreeNode]) -> bool:
     def height(node):
--- Python_NeetCode_150/Easy/Problem_1_Balanced_Binary_Tree/test_solution.py
+++ Python_NeetCode_150/Easy/Problem_1_Balanced_Binary_Tree/test_solution.py
@@ -1,21 +1,5 @@
-import sys
-import os
 import unittest
-
-
-# Print for debugging
-import sys
-import os
-print("test_solution.py")
-print("Absolute path of test file:", os.path.abspath(__file__))
-print("Directory of test file:", os.path.dirname(os.path.abspath(__file__)))
-print("Parent directory:", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print("Sys path:", sys.path)
-
-# Add the project root directory (Python_NeetCode_150) to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Python_NeetCode_150')))
-
-from Python_NeetCode_150.Common.binary_tree import create_tree_from_list
+from .binary_tree import *
 from Python_NeetCode_150.Easy.Problem_1_Balanced_Binary_Tree.solutions import tree_is_balanced_recursive, tree_is_balanced_iterative
 
 class TestTreeBalance(unittest.TestCase):
