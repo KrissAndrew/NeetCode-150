566e02a4f7263d5b58c66faf682e10d7d76c01ce
Kriss Andrew <Kriss1Andrew@gmail.com>
2025-02-01 13:21:39 +1000

    Added pdf documents for all solutions explaining process, finished medium 3 encode decode strings

--- Python_NeetCode_150/Arrays_and_Hashing/Easy/Problem_1_Contains_Duplicate/solutions.py
+++ Python_NeetCode_150/Arrays_and_Hashing/Easy/Problem_1_Contains_Duplicate/solutions.py
@@ -1,4 +1,3 @@
-# O(n) time complexity - the set can look up variables
 def contains_duplicate(nums: list[int]) -> bool:
     seen = set()
     for num in nums:
@@ -6,6 +5,5 @@ def contains_duplicate(nums: list[int]) -> bool:
         seen.add(num)
     return False
 
-# Clever one liner - if the lengths of a set of a list is not equal to the list, the list contains duplicates 
 def contains_duplicate_one_line(nums: list[int]) -> bool:
     return (len(set) < len(nums))
---
+++ Python_NeetCode_150/Arrays_and_Hashing/Medium/Problem_3_Encode_Decode_Strings/solutions.py
@@ -0,0 +1,57 @@
+from typing import List
+
+# Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+def encode_strings(strs: List[str]) -> str:
+    delimiter = '#'
+    encoded = ''.join(f"{delimiter}{len(s)}{delimiter}{s}" for s in strs)
+    return encoded
+
+def decode_strings(encoded: str) -> List[str]:
+    string_array = []
+    i = 0
+
+    while i < len(encoded):
+            if (encoded[i] == '#'):
+                 i += 1            
+            
+            digit = ''
+            while (encoded[i].isdigit()):
+                digit += encoded[i] 
+                i += 1
+            string_len = int(digit)
+            
+            i += 1
+            string_array.append(encoded[i:i + string_len]) 
+            i += string_len
+        
+    return string_array
+
+import struct
+
+def encode_binary(strs: List[str]) -> str:
+    result = b''
+    for s in strs:
+        encoded_length = struct.pack('>I', len(s))
+        result += encoded_length + s.encode('utf-8')
+    return result
+
+def decode_binary(encoded: str) -> List[str]:
+    string_array = []
+    i = 0
+
+    while i < len(encoded):
+        length = struct.unpack('>I', encoded[i:i + 4])[0]
+        i += 4
+        string_array.append(encoded[i:i + length].decode('utf-8'))
+        i += length
+    return string_array
+
+if __name__ == "__main__":
+    Output = [""]
+
+    encoded = encode_strings([""])
+    print(encoded)
+    decoded = decode_strings(encoded)
+    print(decoded)
+    print("Result is good" if decoded == Output else "Result is bad")
