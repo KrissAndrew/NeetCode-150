4317d1b2c3dd86925bf439f013ef57d69e39ca85
Kriss Andrew <Kriss1Andrew@gmail.com>
2025-03-07 20:28:55 +1000

    implemented trapping rain water solution - TO BE IMPROVED - NOT EFFICENT WITH LARGE HEIGHTS

--- Python_NeetCode_150/_2_Two_Pointers/Hard/Problem_1_Trapping_Rain_Water/solutions.py
+++ Python_NeetCode_150/_2_Two_Pointers/Hard/Problem_1_Trapping_Rain_Water/solutions.py
@@ -2,28 +2,26 @@
 # Return the maximum amount of water that can be contained between the heights.
 
 def trapping_rain_water(heights: list[int]) -> int:
-    if not heights or len(heights) < 3: # Sizes below 3 cannot hold water beween
+    if not heights:
         return 0
 
-    trapped = 0
-    counter = 0
-    left, right = 1, 2
+    volume = 0
+    max_height = max(heights)
 
-
-
-    while left != len(heights) and right < len(heights):
-        left_height, right_height = heights[left], heights[right] 
-        while right_height < left_height:
-             right += 1
-             right_height = heights[right]
-        if left == right: right += 1
-        while left != right and right < len(heights):
-             prev_peak = left_height
-             left += 1
-             left_height = heights[left]
-             trapped += prev_peak - left_height
-             
-    return trapped
+    # Process towers row by row from max heights down to 1.
+    for level in range(max_height, 0, -1):
+        first = False
+        count = 0
+        for h in heights:
+            if h >= level:
+                if first:
+                    volume += count
+                    count = 0
+                first = True
+            else:
+                if first:
+                    count += 1
+    return volume
 
 if __name__ == "__main__":
         heights = [0,2,0,3,1,0,1,3,2,1]
@@ -32,5 +30,6 @@ if __name__ == "__main__":
         print(result)
 
         heights = [0,1,0,1,0]
-
-
+        # answer is 9
+        result = trapping_rain_water(heights)
+        print(result)
