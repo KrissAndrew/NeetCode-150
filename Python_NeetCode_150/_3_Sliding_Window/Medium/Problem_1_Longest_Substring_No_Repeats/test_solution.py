66d277e5b9566b7e8c8a2e1d1b9902a099482812
Kriss Andrew <Kriss1Andrew@gmail.com>
2025-03-12 16:18:11 +1000

    completed longest non dupe substring tests

--- Python_NeetCode_150/_3_Sliding_Window/Medium/Problem_1_Longest_Substring_No_Repeats/test_solution.py
+++ Python_NeetCode_150/_3_Sliding_Window/Medium/Problem_1_Longest_Substring_No_Repeats/test_solution.py
@@ -1,24 +1,26 @@
 import unittest
-from .solutions import max_profit
+from .solutions import longest_non_repeat_substring
 
 class TestStockPurchase(unittest.TestCase):
 
     test_cases = [
-        ([10, 3, 7, 4, 1], 4),       # max profit is 7 - 3 = 4
-        ([1, 2, 3, 4, 5, 6], 5),     # max profit is 6 - 1 = 5
-        ([1, 2], 1),                 # max profit is 2 - 1 = 1
-        ([], 0),                     # empty list returns 0 profit
-        ([5], 0),                    # single element returns 0 profit
+        ("abcabcbb", 3), #  1
+        ("zxyzxyz", 3),  #  2
+        ("xxxx", 1),     #  3
+        ("aabbcc", 2),   #  4
+        ("abcabcbb", 3), #  5
+        ("au", 2),       #  6
+        ("aab", 2),      #  7
     ]
         
-    def test_buy_sell_stock(self):
+    def test_longest_non_repeat_substring(self):
         print("\nRunning tests for 'Best Time to Buy and Sell Stock'")
-        for idx, (stock_data, expected) in enumerate(self.test_cases, start=1):
+        for idx, (input, expected) in enumerate(self.test_cases, start=1):
             with self.subTest(test=idx):
-                result = max_profit(stock_data)
+                result = longest_non_repeat_substring(input)
                 self.assertEqual(
                     result, expected,
-                    f"Test case {idx} failed: stock_data={stock_data}, expected={expected} but got {result}"
+                    f"Test case {idx} failed: input={input}, expected={expected} but got {result}"
                 )
 
 if __name__ == '__main__':
