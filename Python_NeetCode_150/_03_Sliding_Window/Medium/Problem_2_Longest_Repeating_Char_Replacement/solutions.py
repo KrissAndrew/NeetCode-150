7129f1e1ad9b6cc45849a22e31817c44ab6bcd39
Kriss Andrew <Kriss1Andrew@gmail.com>
2025-03-20 16:40:42 +1000

    added test case, begun longest repeat solution

--- Python_NeetCode_150/_03_Sliding_Window/Medium/Problem_2_Longest_Repeating_Char_Replacement/solutions.py
+++ Python_NeetCode_150/_03_Sliding_Window/Medium/Problem_2_Longest_Repeating_Char_Replacement/solutions.py
@@ -1,26 +1,31 @@
-# Given a string s, find the length of the longest substring without duplicate characters.
-
-# A substring is a contiguous sequence of characters within a string.
+# Given a string, s, and an int, k, find the length of the longest substring of duplicate characters.
+# You may replace k characters any uppercase char to lengthen substring.
 
 def longest_repeating_char_replacement(s: str, k: int) -> int:
-    if len(set(s)) == len(s): # If all characters in string are unique, then replacement is irrelivent
+    s_list = list(s)
+    s_len = len(s)
+    if len(set(s)) == s_len: # If all characters in string are unique, then replacement is irrelivent
          return 1 + k
     
-    result = 0
+    if k >= s_len: # If k covers the entire string then the longest 
+         return s_len
+    
+    left, right = 0, 1
 
-
-
+    while s_list[left] != s_list[right]:
+         left += 1
 
     return result
          
 
 if __name__ == "__main__":
     test_cases = [
-        (("ABA", 1), 3),     #  1 - Can replace one for "AAA" = len 3
-        (("XYYX", 2), 4),    #  1 - Can replace two for "XXXX" = len 4
+        (("ABC", 1), 2),     #  1 - Can replace one for "(AA)C"|"A(BB)"|"A(CC)" = len 3
+        (("ABA", 1), 3),     #  1 - Can replace one for "(AAA)" = len 3
         (("XYYX", 1), 3),    #  1 - Can replace one for "X(YYY)"|"(YYY)X" = len 3
+        (("XYYX", 2), 4),    #  1 - Can replace two for "(XXXX)" = len 4
         (("AAABABB", 1), 5), #  2 - Can replace one for "(AAAAA)BB" = len 5
-        (("AAABABB", 2), 6), #  2 - Can replace Two for "(AAAAAA)B" = len 5 
+        (("AAABABB", 2), 6), #  2 - Can replace Two for "(AAAAAA)B" = len 6
     ]
 
     all_passed = True
